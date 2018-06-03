36dc0630c66f09eec6fe85331ddbd49ee03f7c20
Anna Jankowicz <reien.san@gmail.com>
2018-06-03 15:42:27 +0200

    Generated components: login, signup, dashboard, new-game, table, chat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NewGameComponent } from './new-game/new-game.component';
+import { TableComponent } from './table/table.component';
+import { ChatComponent } from './chat/chat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    DashboardComponent,
+    NewGameComponent,
+    TableComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule
